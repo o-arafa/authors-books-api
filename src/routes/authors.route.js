078eacb8b2f2c71efbb6a36417177dd3ded82e5c
Obeida Arafa <obeidaarafa@gmail.com>
2025-09-07 19:58:58 +0300

    feat: add ObjectId validation middleware for route parameters

--- src/routes/authors.route.js
+++ src/routes/authors.route.js
@@ -1,10 +1,23 @@
 const express = require("express");
 const router = express.Router();
 const authorsController = require("../controllers/authors.controller");
+const validateObjectId = require("../middlewares/validateObjectId");
 
 router.get("/", authorsController.getAllAuthors);
-router.get("/:authorId", authorsController.getAuthor);
+router.get(
+  "/:authorId",
+  validateObjectId("authorId"),
+  authorsController.getAuthor
+);
 router.post("/", authorsController.createAuthor);
-router.put("/:authorId", authorsController.updateAuthor);
-router.delete("/:authorId", authorsController.deleteAuthor);
+router.put(
+  "/:authorId",
+  validateObjectId("authorId"),
+  authorsController.updateAuthor
+);
+router.delete(
+  "/:authorId",
+  validateObjectId("authorId"),
+  authorsController.deleteAuthor
+);
 module.exports = router;
--- src/routes/books.route.js
+++ src/routes/books.route.js
@@ -1,11 +1,16 @@
 const express = require("express");
 const router = express.Router({ mergeParams: true });
 const booksController = require("../controllers/books.controller");
+const validateObjectId = require("../middlewares/validateObjectId");
 
 router.get("/", booksController.getAllBooks);
-router.get("/:bookId", booksController.getBook);
+router.get("/:bookId", validateObjectId("bookId"), booksController.getBook);
 router.post("/", booksController.createBook);
-router.put("/:bookId", booksController.updateBook);
-router.delete("/:bookId", booksController.deleteBook);
+router.put("/:bookId", validateObjectId("bookId"), booksController.updateBook);
+router.delete(
+  "/:bookId",
+  validateObjectId("bookId"),
+  booksController.deleteBook
+);
 
 module.exports = router;
